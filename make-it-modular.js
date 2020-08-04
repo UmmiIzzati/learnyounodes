fcc03a050cbbe2dc2a55692efced4964b3453477
Ummi Izzati <ummizati95@gmail.com>
2020-08-04 10:11:40 +0800

    first commit

---
+++ make-it-modular.js
@@ -0,0 +1,12 @@
+const myModule = require('./mymodule.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+myModule(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err)
+
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
---
+++ mymodule.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+var path = require('path')
+
+module.exports = function(dir, fileString, callback) {
+		
+		fs.readdir(dir, function (err, list) {
+			
+			if (err)
+				return callback(err)
+			
+			list = list.filter(function (file) {
+				return path.extname(file) === '.' + fileString
+			})
+			callback(null, list)
+		})
+	}
