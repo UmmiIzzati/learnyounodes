00258ab90d482d0961c26c46fd0029937fd1ea1e
Ummi Izzati <ummizati95@gmail.com>
2020-08-04 10:45:24 +0800

    first commit

---
+++ http-file-server.js
@@ -0,0 +1,7 @@
+const http = require('http')
+var fs = require('fs')
+const server = http.createServer(function (req, res) {
+	res.writeHead(200, {'content-type':'text/plain'})
+	fs.createReadStream(process.argv[3]).pipe(res)
+})
+server.listen(Number(process.argv[2]))
